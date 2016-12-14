dee617afaedf1ed7dd2a9cebf730dafdbe24e507
Théo Poizat <theo.poizat@etu.unistra.fr>
2016-12-14 18:26:48 +0100

    utilisateur.js

--- config/passport.js
+++ config/passport.js
@@ -9,76 +9,76 @@ var connection = mysql.createConnection(db.connection);
 
 module.exports = function(passport) {
 
-    // configuration des sessions Passport
-    passport.serializeUser(function(user, done) {
-        done(null, user.Login); // user.Login = clé primaire
+  // configuration des sessions Passport
+  passport.serializeUser(function(user, done) {
+    done(null, user.Login); // user.Login = clé primaire
+  });
+
+  passport.deserializeUser(function(id, done) {
+    connection.query("SELECT * FROM UTILISATEUR WHERE Login = ? ",[id], function(err, rows){
+      done(err, rows[0]);
     });
+  });
 
-    passport.deserializeUser(function(id, done) {
-        connection.query("SELECT * FROM UTILISATEUR WHERE Login = ? ",[id], function(err, rows){
-            done(err, rows[0]);
-        });
-    });
+  // inscription
+  passport.use(
+    'local-signup',
+    new LocalStrategy({
+        // le nom des champs qu'on récupère dans le formulaire
+        usernameField : 'Login',
+        passwordField : 'Mdp',
+        passReqToCallback : true
+    },
+    function(req, username, password, done) {
+      // on vérifie si l'utilisateur existe déjà
+      connection.query("SELECT * FROM UTILISATEUR WHERE Login = ?",[username], function(err, rows) {
+        if (err) // échec de la requête
+            return done(err);
+        if (rows.length) { // l'utilisateur existe déjà
+            return done(null, false);
+        } else { // l'utilisateur n'existe pas
 
-    // inscription
-    passport.use(
-        'local-signup',
-        new LocalStrategy({
-            // le nom des champs qu'on récupère dans le formulaire
-            usernameField : 'Login',
-            passwordField : 'Mdp',
-            passReqToCallback : true
-        },
-        function(req, username, password, done) {
-            // on vérifie si l'utilisateur existe déjà
-            connection.query("SELECT * FROM UTILISATEUR WHERE Login = ?",[username], function(err, rows) {
-                if (err) // échec de la requête
-                    return done(err);
-                if (rows.length) { // l'utilisateur existe déjà
-                    return done(null, false);
-                } else { // l'utilisateur n'existe pas
+            // on crée un nouvel utilisateur
+          var nouveUtilisateur = {
+            Login: username,
+            Mdp: bcrypt.hashSync(password, null, null)  // mot de passe en paramètre haché
+          };
 
-                    // on crée un nouvel utilisateur
-                    var nouveUtilisateur = {
-                        Login: username,
-                        Mdp: bcrypt.hashSync(password, null, null)  // mot de passe en paramètre haché
-                    };
+          // préparation de la requête et insertion de l'utilisateur
+          var insertQuery = "INSERT INTO UTILISATEUR (Login, Mdp) values (?,?)";
 
-                    // préparation de la requête et insertion de l'utilisateur
-                    var insertQuery = "INSERT INTO UTILISATEUR ( Login, Mdp ) values (?,?)";
+          connection.query(insertQuery, [nouveUtilisateur.Login, nouveUtilisateur.Mdp], function(err, rows) {
+            return done(null, nouveUtilisateur);
+          });
+        }
+      });
+    })
+  );
 
-                    connection.query(insertQuery,[nouveUtilisateur.Login, nouveUtilisateur.Mdp], function(err, rows) {
-                        return done(null, nouveUtilisateur);
-                    });
-                }
-            });
-        })
-    );
+  // connexion
+  passport.use(
+    'local-login',
+    new LocalStrategy({
+      // toujours le nom des champs qu'on récupère dans le formulaire
+      usernameField : 'Login',
+      passwordField : 'Mdp',
+      passReqToCallback : true
+    },
+    function(req, username, password, done) {
+      connection.query("SELECT * FROM UTILISATEUR WHERE Login = ?",[username], function(err, rows){
+        if (err) // échec de la requête
+          return done(err);
+        if (!rows.length) { // l'utilisateur n'existe pas
+          return done(null, false);
+        }
 
-    // connexion
-    passport.use(
-        'local-login',
-        new LocalStrategy({
-            // toujours le nom des champs qu'on récupère dans le formulaire
-            usernameField : 'Login',
-            passwordField : 'Mdp',
-            passReqToCallback : true
-        },
-        function(req, username, password, done) {
-            connection.query("SELECT * FROM UTILISATEUR WHERE Login = ?",[username], function(err, rows){
-                if (err) // échec de la requête
-                    return done(err);
-                if (!rows.length) { // l'utilisateur n'existe pas
-                    return done(null, false);
-                }
+        // le mot de passe ne correspond pas
+        if (!bcrypt.compareSync(password, rows[0].Mdp))
+          return done(null, false);
 
-                // le mot de passe ne correspond pas
-                if (!bcrypt.compareSync(password, rows[0].Mdp))
-                    return done(null, false);
-
-                // on renvoie l'utilisateur connecté
-                return done(null, rows[0]);
-            });
-        })
-    );
+        // on renvoie l'utilisateur connecté
+        return done(null, rows[0]);
+      });
+    })
+  );
 };
