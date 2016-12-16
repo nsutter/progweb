7986e9880364254ca371c1facc99015df6331ba9
nsutter <nsutter@etu.unistra.fr>
2016-12-16 14:42:56 +0100

    [Feature] 404

--- app.js
+++ app.js
@@ -55,7 +55,14 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+	if(err.status == 404)
+  {
+    res.render('404');
+  }
+	else
+	{	
+		res.render('error');
+	}
 });
 
 module.exports = app;
