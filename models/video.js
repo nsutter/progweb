d77ec6253c1eb4bdd866b77e65852610ad4b2b9f
Théo Poizat <theo.poizat@etu.unistra.fr>
2016-12-16 12:24:11 +0100

    recommendation

--- models/video.js
+++ models/video.js
@@ -63,12 +63,6 @@ module.exports =
     });
   },
 
-  // supprime une vidéo en fonction d'IdVideo
-  deleteOneById(id)
-  {
-    connection.query("DELETE FROM VIDEO WHERE IdVideo = ?", [id], function(err, rows){});
-  },
-
   // récupère les vidéos diffusées il y a - de 2 semaines des émissions auxquelles l'utilisateur est abonné
   getNouveautes: function(callback, login)
   {
@@ -77,12 +71,29 @@ module.exports =
     });
   },
 
-  // récupère les vidéos favoris de l'utilisateur
+  // récupère les vidéos favoris de l'utilisateur login
   getFavoris: function(callback, login)
   {
     connection.query("SELECT * FROM FAVORIS f, VIDEO v WHERE f.Login = ? AND f.IdVideo = v.IdVideo", [login], function(err, rows){
       callback(err, rows);
     });
+  },
+
+  // récupère les vidéos recommandées de l'utilisateur login
+  getPopulars: function(callback, login)
+  {
+    console.log(login);
+
+    connection.query("SELECT *, COUNT(*) FROM VIDEO v, CATEGORIEU c, HISTORIQUE h WHERE c.Login = ? AND c.categorie = v.categorie AND v.IdVideo = h.IdVideo GROUP BY v.IdVideo ORDER BY COUNT(*) DESC", [login], function(err, rows)
+    {
+      console.log(rows);
+      callback(err, rows);
+    })
+  },
+
+  addHistorique: function(id, login)
+  {
+    connection.query("INSERT INTO HISTORIQUE (IdVideo, DateVisionnage, Login) VALUES (?, CURDATE(), ?)", [id, login], function(err, rows){})
   },
 
   // INSERTION
@@ -98,12 +109,6 @@ module.exports =
     });
   },
 
-  // supprime un favori en fonction du Login et d'IdVideo
-  deleteFavori: function(Login, IdVideo)
-  {
-    connection.query("DELETE FROM FAVORIS WHERE Login = ? AND IdVideo = ?", [Login, IdVideo], function(err, rows){});
-  },
-
   // insère un abonnement pour l'utilisateur Login
   setAbonnement: function(Login, Mdp, NomEmission)
   {
@@ -114,4 +119,18 @@ module.exports =
       }
     });
   },
+
+  // SUPPRESSION
+
+  // supprime une vidéo en fonction d'IdVideo
+  deleteOneById(id)
+  {
+    connection.query("DELETE FROM VIDEO WHERE IdVideo = ?", [id], function(err, rows){});
+  },
+
+  // supprime un favori en fonction du Login et d'IdVideo
+  deleteFavori: function(Login, IdVideo)
+  {
+    connection.query("DELETE FROM FAVORIS WHERE Login = ? AND IdVideo = ?", [Login, IdVideo], function(err, rows){});
+  }
 }
